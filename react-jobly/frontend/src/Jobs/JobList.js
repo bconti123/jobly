39c5c84cff99e7b17b1d14b621dbe861048088f0
bconti123 <bryantcconti@gmail.com>
2023-11-11 22:40:38 -0800

    Set up Jobs

--- react-jobly/frontend/src/Jobs/JobList.js
+++ react-jobly/frontend/src/Jobs/JobList.js
@@ -3,9 +3,14 @@ import React from "react";
 const JobList = ({ jobs }) => {
   return (
     <div>
-        {jobs.map(job => (
-            <h3>{job.title}</h3>
-        ))}
+      {jobs.map((job) => (
+        <div key={job.id}>
+          <h3>{job.title}</h3>
+          <p>Salary: </p>
+          <p>Equity: </p>
+          {/* <input type="submit" value={IsApplied ? "Apply" : "Applied"} onClick={"hold"}/> */}
+        </div>
+      ))}
     </div>
   );
 };
--- react-jobly/frontend/src/Jobs/Jobs.js
+++ react-jobly/frontend/src/Jobs/Jobs.js
@@ -1,11 +1,24 @@
-
+import React, { useState, useEffect } from "react";
+import JoblyApi from "../api";
+import JobList from "./JobList";
 
 const Jobs = () => {
-    return (
-        <div>
-            Jobs
-        </div>
-    )
-}
+  const [jobs, setJobs] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await JoblyApi.getJobs();
+        setJobs(response)
+      } catch (err) {
+        console.error(err)
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (!jobs) return <p>Loading...</p>;
+  return (<div><JobList jobs={jobs} /></div>);
+};
 
 export default Jobs;
